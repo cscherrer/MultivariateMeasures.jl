b8567187af47c20cab8049cbd9df43cdf67b5ca9
Chad Scherrer <chad.scherrer@gmail.com>
2021-08-06 09:55:36 -0700

    make it work

--- src/MultivariateMeasures.jl
+++ src/MultivariateMeasures.jl
@@ -1,5 +1,15 @@
 module MultivariateMeasures
 
+
+using Static
+using ArrayInterface
+
+struct KnownSize{S, T}
+    value::T
+end
+
+KnownSize(x::T) where {T} = KnownSize{Tuple{ArrayInterface.known_size(T)...}, T}(x)
+
 using MeasureTheory
 using KeywordCalls
 include("mvnormal.jl")
