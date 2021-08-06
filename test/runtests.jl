985c1b25b8158b6635c7fcefefc31d25de4a048e
Chad Scherrer <chad.scherrer@gmail.com>
2021-08-06 16:03:24 -0700

    move LKJCholesky test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,19 @@ using MultivariateMeasures
 using Test
 
 @testset "MultivariateMeasures.jl" begin
-    # Write your tests here.
+
+    @testset "LKJCholesky" begin
+        D = LKJCholesky{(:k,:η)}
+        par = transform(asparams(D, (k=4,)), randn(1))
+        d = D(merge((k=4,),par))
+        # @test params(d) == par
+
+        η  = par.η
+        logη = log(η)
+
+        y = rand(d)
+        η = par.η
+        ℓ = logdensity(LKJCholesky(4,η), y)
+        @test ℓ ≈ logdensity(LKJCholesky(k=4,logη=logη), y)
+    end
 end
